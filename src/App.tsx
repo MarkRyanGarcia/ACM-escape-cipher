e61e923787ade470b0c0a3fad90ad07144e6664f
Mark Garcia <markrgarcia@Marks-Air.lan>
2024-11-20 22:56:04 -0800

    removed unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import CipherPage from "./routes/cipher/cipher.tsx";
 import HomePage from "./routes/homepage/homepage.tsx";
 
